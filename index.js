e08afcb41a3282fd5ac4d378ce79f0dd3d6b2b78
piperal <piperal@tlu.com>
2025-09-29 11:20:25 +0300

    express server

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log("test")
+const express = require('express')
+const app = express()
+const port = 5014
+
+app.get("/", (req,res)=>{
+    res.send("Hello")
+
+})
+
+app.listen(port,()=>{
+    console.log("Server running on " + port)
+})
